07451f445781167167520ddfb0a9a70a15692853
Hast11 <andres2000gil@gmail.com>
2023-11-18 13:32:45 +0100

    spinner activo mientras isLoading

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Header from './Header';
 import SpinnerCarga from './SpinnerCarga';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [isLoading, setLoading] = useState(true)
+
   return (
     <div className="App">
       <Header />
-      <SpinnerCarga />
+      {(isLoading) && <SpinnerCarga />} {/* Carga SpinnerCarga mientras is loading sea true */}
     </div>
   );
 }
