0b67f9bf0659746985f96b3fa695991d26c270a6
Hast11 <andres2000gil@gmail.com>
2023-11-18 13:40:49 +0100

    esqueleto searchPage

---
+++ src/SearchPage.js
@@ -0,0 +1,8 @@
+export default function SearchPage (props){
+    return(
+        <div id="searchPage">
+            <h2 id="catalogo">Catálogo</h2>
+            <input id="filtro"></input>
+            <button id="buscador"></button>
+        </div>
+)}
