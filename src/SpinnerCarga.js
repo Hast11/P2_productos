f4a8dadc3b9194593b1fd729c003139dc91b4795
Hast11 <andres2000gil@gmail.com>
2023-11-18 13:14:44 +0100

    spinnerCarga

--- src/SpinnerCarga.js
+++ src/SpinnerCarga.js
@@ -1,10 +1,14 @@
 import Spinner from 'react-bootstrap/Spinner';
+import './SpinnerCarga.css'
 
 function SpinnerCarga() {
   return (
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
+    <div className='Spinner'>
+      <link href="SpinnerCarga.css" rel="stylesheet"></link>
+      <Spinner animation="border" variant="primary" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
   );
 }
 
