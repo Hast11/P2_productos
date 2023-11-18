54d0067c83cc69d2a27fb34cb2c763e7dc099975
Hast11 <andres2000gil@gmail.com>
2023-11-18 13:41:06 +0100

    meterSearchaPage en app

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Header from './Header';
 import SpinnerCarga from './SpinnerCarga';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {useState} from 'react';
+import SearchPage from './SearchPage';
 
 function App() {
 
   const [isLoading, setLoading] = useState(true)
+  const [productos, setProductos] = useState(null)
 
   return (
     <div className="App">
       <Header />
       {(isLoading) && <SpinnerCarga />} {/* Carga SpinnerCarga mientras is loading sea true */}
+      {(!isLoading) && <SearchPage theproducts={productos} />}
     </div>
   );
 }
