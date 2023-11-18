e4784f9ba09164ef5f71b43f81c6962367b2157f
Hast11 <andres2000gil@gmail.com>
2023-11-18 14:22:29 +0100

    meter en cajones

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,13 +1,20 @@
 import App from "./App"
-
+import './Style/SearchPage.css';
 
 export default function SearchPage (props){
     return(
         <div id="searchPage">
             <h2 id="catalogo">Catálogo</h2>
-            <input id="filtro" placeholder="escriba lo que quiera buscar"></input>
-            <div>
-                <button id="buscador">buscar</button>
+            <div id="buscadorFiltro">
+                <div id="buscador">Buscar
+                    <input id="filtro" placeholder="escriba lo que quiera buscar" ></input>
+                    <div>
+                        <button id="buscador">buscar</button>
+                    </div>
+                </div>
+            </div>
+            <div id="filter">
+                <select></select>
             </div>
         </div>
 )}
