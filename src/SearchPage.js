27fde925d4d44af6b384f3c9f0ba5a2f7c4d4401
Hast11 <andres2000gil@gmail.com>
2023-11-19 19:01:09 +0100

    terminar searchPage, contiene la version antigua lo corregido

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,55 +1,76 @@
-import App from "./App"
 import './Style/SearchPage.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useState } from "react";
 import Form from 'react-bootstrap/Form';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 export default function SearchPage (props){ /* Recibe como props theproducts y */ 
     
 const [busqueda, setBusqueda] = useState(null);
-const datos = props.theproducts;
 const [resultados, setResultados] = useState(props.theproducts);
 
-/*function filtrarBusqueda(query) {
-    setResultados(datos.filter((el) => el.title.toLowerCase().includes(query.toLowerCase()) /* | el.description.toLowerCase().includes(query.toLowerCase()) ))   }*/
+var categorias = props.theproducts.reduce((unique, item) => (unique.includes(item.category) ? unique : [...unique, item.category]),[],); 
+/*Anotar esto, es una función que sirve para guardar las categorías en un array */
+
+function filtrarBusqueda(){
+    if (busqueda===null){
+        setResultados(props.theproducts)
+    }else{
+        setResultados(props.theproducts.filter(word => word.title.toLowerCase().includes(filtro.toLowerCase())));
+    }
+}
+
+function selector(){
+    var select = document.getElementById('selector');
+    var value = select.options[select.selevtedIndex].value;
+    if(value !== "All"){
+        setResultados(props.theproducts.filter(word=>(word.category===(value))));
+    }
+    else{
+        setResultados(props.theproducts);
+    }
+}
 
 return(
-        <div id="searchPage">
-            <h2 id="catalogo">Catálogo</h2>
+    <div id="searchPage">
+        <h2 id="catalogo">Catálogo</h2>
 
-            <div id="Formulario">
-                <div id="buscadorFiltro">
-                    <div id="buscador"><b>Buscar</b>
-                        <input id="inputBusqueda" placeholder="Escriba lo que quiera buscar..." value={busqueda} onChange={(e)=>setBusqueda(e.target.value)}></input>
-                        <div>
-                            <button id="buscador" /* onClick={filtrarBusqueda(busqueda)} */>Buscar</button>
-                        </div>
+        <div id="Formulario">
+            <div id="buscadorFiltro">
+                <div id="buscador"><b>Buscar</b>
+                    <input id="inputBusqueda" placeholder="Escriba lo que quiera buscar..." onChange={(e)=>setBusqueda(e.target.value)}></input>
+                    <div>
+                        <button id="buscador" onClick={filtrarBusqueda()}>Buscar</button>
                     </div>
                 </div>
-
-                <Form.Select aria-label="Default select example" id="selector">
-                    <option>Open this select menu</option>
-                    <option value="All">All</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </Form.Select>
             </div>
 
-            {/* <div id="“productosresultados”"> pendiente cambiar a que sacamos por pantalla resultados
-                {props.theproducts.map((product,index) => {
-                    <Card style={{ width: '18rem' }} className="unproducto" key={index}>
-                    <Card.Img variant="top" src={product.images[0]} />
-                    <Card.Body>
-                        <Card.Title>{product.title}</Card.Title>
-                        <Card.Text>{product.description}</Card.Text>
-                        <Button variant="primary">VER</Button>
-                    </Card.Body>
-                    </Card>
-                }
-                )}
-            </div> */}
-
-
+            <div className='sel'><b>Seleccionar</b>
+                <select id="selector" defaultValue="All" onChange={()=>selector()}>
+                    <option value="All" > All </option>
+                    {categorias.sort().map((item, index) => 
+                    <option key={index} value={item}> {item} </option>
+                    )}
+                </select>
+            </div>
         </div>
-)}
+        <ul>
+            <CardGroup>{resultados.map((item, index) =>
+                <li key={index} className="unproducto">
+                    <Card style={{ width: '18rem '}} bg="secondary" text="light" border="dark">
+                        <Card.Img  variant="top" src={item.thumbnail}/>
+                            <Card.Body>
+                                <Card.Title className="text1">{item.title}</Card.Title>
+                                <Card.Text className="text">{item.description}</Card.Text>
+                                <Link to={'/products/'+ index }> <Button variant="primary" onClick={()=>props.funcion(item.id-1)}> VER </Button> </Link>
+                            </Card.Body>
+                    </Card>
+                </li>)}
+            </CardGroup>
+        </ul>
+
+
+    </div>
+)
+}
