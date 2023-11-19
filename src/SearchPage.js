59836a65fe8b849e9fb28a334ea787cfe9c1fe86
Hast11 <andres2000gil@gmail.com>
2023-11-19 10:52:25 +0100

    Dos contenedores azules!!

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -5,16 +5,20 @@ export default function SearchPage (props){
     return(
         <div id="searchPage">
             <h2 id="catalogo">Catálogo</h2>
-            <div id="buscadorFiltro">
-                <div id="buscador">Buscar
-                    <input id="filtro" placeholder="escriba lo que quiera buscar" ></input>
-                    <div>
-                        <button id="buscador">buscar</button>
+
+            <div id="Centremonos">
+                <div id="buscadorFiltro">
+                    <div id="buscador">Buscar
+                        <input id="input" placeholder="Escriba lo que quiera buscar..." ></input>
+                        <div>
+                            <button id="buscador">buscar</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div id="filter">
-                <select></select>
+
+                <div id="filter">
+                    <select></select>
+                </div>
             </div>
         </div>
 )}
