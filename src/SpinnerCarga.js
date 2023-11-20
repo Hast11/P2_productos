9c434a83df0ffaeba119941c11bcb1eb04f1745c
Hast11 <andres2000gil@gmail.com>
2023-11-20 12:16:37 +0100

    ver para creer

--- src/SpinnerCarga.js
+++ src/SpinnerCarga.js
@@ -6,7 +6,7 @@ const spinnerWrapperEl = document.querySelector('.Spinner');
 function SpinnerCarga() {
   return (
     <div className='Spinner'>
-      <Spinner animation="border" variant="primary" role="status">
+      <Spinner animation="border" variant="primary" role="status" id="loading" className='Spinner'>
         <span className="visually-hidden">Loading...</span>
       </Spinner>
     </div>
